94f6ae3fbe19eef575d1f82338365e19963eec38
Konstantin <k1st.volkov@gmail.com>
2023-08-23 15:24:09 +0300

    Реализован метод для редактирования записей, добавлено короткое описание для каждого из мтеодов

--- phonebook.py
+++ phonebook.py
@@ -1,16 +1,24 @@
-from itertools import count
 import json
-import random
-
-from constants import *
+from typing import List, Dict, Union
 
 
 class PhoneBook:
-    def __init__(self, file_path) -> None:
+    
+    """
+    Класс представляет собой Телефонную записную книжку с набором функционала в виде методов.
+    """
+    
+    def __init__(self, file_path: str) -> None:
         self.file_path = file_path
         self.data = self.load_data()
         
-    def load_data(self):
+    def load_data(self) -> List[Dict[str, Union[str, int]]]:
+        """ Метод служит для прочтения JSON файла и дальнейшей работы в нем. Принимает на вход путь к файлу
+        который указывается при инициализации класса.
+
+        Возвращает:
+            List[Dict[str, Union[str, int]]]: Возвращает все имеющиеся контакты или информацию, что файл не найден
+        """
         try:
             with open(self.file_path, "r", encoding='utf-8') as file:
                 data = json.load(file)
@@ -19,69 +27,126 @@ class PhoneBook:
             print("Файл не найден или содержит некорректные данные. Создание нового телефонного справочника...")
             return []
 
-    def save_data(self):
+    def save_data(self) -> Exception | None:
+        """Метод служит для записи данных в JSON файл, с помощью которого был инициализирован класс.
+
+        Возвращает:
+            Exception | None:
+        """
         try:
             with open(self.file_path, 'w', encoding='utf-8') as file:
                 json.dump(self.data, file, ensure_ascii=False, indent=4)
         except Exception as error:
             return error
             
-    def display_contacts(self, page: int = 1, contacts_per_page: int = 15):
+    def display_contacts(self, page: int = 1, contacts_per_page: int = 15) -> None:
+        """Метод для постраничного отображения списка контактов
+
+        Аргументы:
+            page (int, optional): Номер страницы, по умолчанию начинаем с 1.
+            contacts_per_page (int, optional): Количество записей на странице, по умолчанию 15.
+        """
         start_index = (page - 1) * contacts_per_page
         end_index = start_index + contacts_per_page
         contacts = self.data[start_index:end_index]
+        if len(contacts) == 0:
+            print("Вы достигли последней страницы.")
+            return
+    
         for contact in contacts:
+            print("--------------------")
+            print(f"Номер записи: {contact['id']}")
             print(f"Фамилия: {contact['Фамилия']}")
             print(f"Имя: {contact['Имя']}")
+            print(f"Отчество: {contact['Отчество']}")
             print(f"Компания: {contact['Компания']}")
-            print(f"Телефонный номер: {contact['Телефонный номер']}")
-            print("--------------------")
+            print(f"Рабочий номер: {contact['Рабочий номер']}")
+            print(f"Личный номер: {contact['Личный номер']}")
 
-    def add_contact(self):
-        print(self.count_contact())
+    def add_contact(self) -> None:
+        """ Метод служит для записи новых контактов в книгу. Формирует словарь с определенными ключами, 
+        сортирует в алфавитном порядке, по ключу "Фамилия" все записи и добавляет новый контакт.
+        """
         contact = {}
         contact['id'] = self.count_contact() + 1
         contact["Фамилия"] = input("Фамилия контакта: ")
         contact["Имя"] = input("Имя контакта: ")
+        contact["Отчество"] = input("Отчество контакта: ")
         contact["Компания"] = input("Компания контакта: ")
-        contact["Телефонный номер"] = int(input("Телефонный номер контакта: "))
+        contact["Личный номер"] = input("Введите личный номер телефона: ")
+        contact["Рабочий номер"] = input("Введите рабочий номер телефона: ")
         self.data.append(contact)
         self.data.sort(key=lambda x: x["Фамилия"])
         self.save_data()
         print("Запись успешно добавлена.")
         
-    def edit_record(self, index):
-        if index < 1 or index > len(self.data):
-            print("Недопустимый индекс записи!")
+    def edit_contact(self, index: int) -> None:
+        """Метод служит для редактирования уже имеющихся контактов
+        Реализован поиск по номеру записи конкретного контакта, изменение всех его полей и сохранения в файл.
+
+        Аргументы:
+            index (int): Номер записи контакта (id)
+        """
+        contact = self.search_contacs(index)
+        if not contact:
+            print("Контакта не существует!")
         else:
-            record = self.data[index - 1]
-            print("Редактирование записи:")
-            record["Фамилия"] = input("Введите фамилию: ")
-            record["Имя"] = input("Введите имя: ")
-            record["Компания"] = input("Введите название компании: ")
-            record["Телефонный номер"] = input("Введите телефон: ")
-            self.data.sort(key=lambda x: x["Фамилия"]) 
-            self.save_data()
-            print("Запись успешно отредактирована!")
+            print(f"Вы хотите отредактировать эту запись?\n {contact}")
+            answer = int(input("1 - Да \n0 - Нет\n "))
+            if answer == 1:
+                print("Редактирование записи:")
+                contact["Фамилия"] = input("Введите фамилию: ")
+                contact["Имя"] = input("Введите имя: ")
+                contact["Отчество"] = input("Введите отчество: ")
+                contact["Компания"] = input("Введите название компании: ")
+                contact["Личный номер"] = input("Введите личный номер телефона: ")
+                contact["Рабочий номер"] = input("Введите рабочий номер телефона: ")
+                self.data.sort(key=lambda x: x["Фамилия"]) 
+                self.save_data()
+                print("Запись успешно отредактирована!")
+            elif answer == 0:
+                print("Выберете другую запись")
+            else:
+                print("Вы выбрали несуществующий вариант, начните сначала")
             
-    def count_contact(self):
+    def count_contact(self) -> int:
+        """Метод служит для подсчета общего количества контактов в записной книге
+
+        Возвращает:
+            int: Возвращает число равное количеству записей
+        """
         return (len(self.data))
 
-    def search_contacs(self, search_criteria):
+    def search_contacs(self, search_criteria: dict) -> Dict[str, str | int] | list:
+        """Метод служит для поиска определенных записией. В зависимсоти от сценария принимает разные аргументы
+
+        Args:
+            search_criteria (_type_): Принимает словарь с полями по которым будет осуществлен поиск.
+            Каждое поле является не обязательным, но необходимо указать хотя бы одно.
+
+        Returns:
+            Dict[str, str | int] | list: Возвращает один или несколько результатов в зависимости от сценария
+        """
         matching_records = []
         for record in self.data:
             match = True
-            for key, value in search_criteria.items():
-                if value.lower() and key in record and record[key].lower() != value.lower():
-                    match = False
-                    break
-            if match:
-                matching_records.append(record)
+            if "id" in search_criteria:
+                if search_criteria["id"] == record["id"]:
+                    return record
+            else:
+                for key, value in search_criteria.items():
+                    if value.lower() and key in record and record[key].lower() != value.lower():
+                        match = False
+                        break
+                if match:
+                    matching_records.append(record)
         return matching_records
     
-    def book_menu(self):
+    def book_menu(self) -> None:
+        """Метод служит для навигации и определения сценария всего класса
+        """
         page_num = 1
-        records_per_page = 10
+        records_per_page = 5
 
         while True:
             print("Меню:")
@@ -95,13 +160,18 @@ class PhoneBook:
 
             if choice == "1":
                 self.display_contacts(page_num, records_per_page)
+                if len(self.data) <= page_num * records_per_page:
+                    print("Вы достигли последней страницы.")
+                    self.book_menu()
                 page_num += 1
             elif choice == "2":
                 self.add_contact()
             elif choice == "3":
                 try:
-                    index = int(input("Введите индекс контакта для редактирования: "))
-                    self.edit_record(index)
+                    search_criteria = {
+                        "id": int(input("Введите номер записи: "))
+                    }
+                    self.edit_contact(search_criteria)
                 except ValueError:
                     print("Недопустимое значение индекса!")
             elif choice == "4":
